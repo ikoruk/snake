56c60146be824086c5873874def56824946c802c
Yuliy Schwartzburg <yuliy.schwartzburg@canonical.com>
2024-08-05 17:23:43 +0200

    bookkeeping

--- Package.swift
+++ Package.swift
@@ -1,18 +1,19 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "snake",
-    dependencies: [
-        .package(url: "https://github.com/pakLebah/ANSITerminal", from: "0.0.3"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "snake",
-            dependencies: ["ANSITerminal"]),
-    ]
+  name: "snake",
+  dependencies: [
+    .package(url: "https://github.com/pakLebah/ANSITerminal", from: "0.0.3")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "snake",
+      dependencies: ["ANSITerminal"],
+      path: "Sources")
+  ]
 )
